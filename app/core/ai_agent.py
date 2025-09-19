77dce8029f56ab433513b2221be86073bc30670c
NASIM <nosytub@gmail.com>
2025-09-19 18:19:37 +0530

    Backend Configured

--- app/core/ai_agent.py
+++ app/core/ai_agent.py
@@ -5,8 +5,10 @@ from langchain_core.messages import AIMessage
 from app.config.settings import settings
 
 def get_response_from_ai_agents(llm_id,query,allow_search,system_prompt):
+    
     llm=ChatGroq(model=llm_id)
     tools=[TavilySearchResults(max_results=2)] if allow_search else []
+
     agent=create_react_agent(
         model=llm,
         tools=tools,
@@ -14,7 +16,9 @@ def get_response_from_ai_agents(llm_id,query,allow_search,system_prompt):
         prompt=system_prompt
     )
     state={"messages":query}
+
     response=agent.invoke(state)
+
     messages=response.get("messages")
     ai_message=[message.content for message in messages if isinstance(message, AIMessage)]
     return ai_message[-1]
